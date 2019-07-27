10652581df203530ee5eeccc5224ed213719c0f2
Ruotong Wu <wrtforwp7@hotmail.com>
2019-07-27 02:10:43 -0400

    remove migration folders

--- todo_list/models.py
+++ todo_list/models.py
@@ -8,6 +8,9 @@ from datetime import date
 class ItemGroup(models.Model):
     deadline = models.DateField(default = date.today)
     owner = models.ForeignKey('authentication.CustomUser', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return str(self.deadline) + '|' + self.owner.username
     
 class ToDoItem(models.Model):
     item = models.CharField(max_length=200)
